d5a2480ef3909c743efc9847bbedaf54b6a225d2
DaveFullStack <119569407+DaveFullStack@users.noreply.github.com>
2023-02-13 15:56:07 +0000

    first part of turtle code

---
+++ pong.py
@@ -0,0 +1,18 @@
+import turtle 
+
+wn = turtle.Screen()
+wn.title("Pong by Big Dave")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)
+wn.tracer(0)
+
+# paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape('square')
+paddle_a.color('green')
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350, 0) 
+
+input()
